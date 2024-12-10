da35b1b1fbde928c9cb99284edcfcfb2af1a18c7
zmx2321 <zmx2321@163.com>
2024-12-10 17:27:47 +0800

    fix: init

---
+++ HelloJavaWeb01/src/cn.edu.tyut/HelloServelt.java
@@ -0,0 +1,11 @@
+package cn.edu.tyut;
+//import jakarta.servlet.http.HttpServlet;
+
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class HelloServelt {
+//    protected void doGet(HttpRequest req, HttpResponse resp) {
+//        printwriter out=resp.getwriter
+//    }
+}
---
+++ HelloJavaWeb02/src/main/java/com/demo/hellojavaweb02/HelloServlet.java
@@ -0,0 +1,26 @@
+package com.demo.hellojavaweb02;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World111222211!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println(message);
+    }
+
+    public void destroy() {
+    }
+}
