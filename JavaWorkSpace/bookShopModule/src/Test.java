0fa4daf82471096990e943662b24a36af6109c9b
zmx2321 <zmx2321@163.com>
2024-12-11 10:03:32 +0800

    fix: init

--- JavaWorkSpace/bookShopModule/src/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        /*
-          程序入口
-         */
-        // 打印菜单
-    }
-}
---
+++ JavaWorkSpace/bookShopModule/src/com/my/book/Test.java
@@ -0,0 +1,24 @@
+package com.my.book;
+
+/**
+ * 书店
+ * 1. 展示书籍
+ * 2. 上新书籍
+ * 3. 下架书籍
+ * 4. 退出应用
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        /*
+          程序入口
+         */
+        // 打印菜单
+        System.out.println("*********************");
+        System.out.println("1. 展示书籍");
+        System.out.println("2. 上新书籍");
+        System.out.println("3. 下架书籍");
+        System.out.println("4. 退出应用");
+        System.out.println("*********************");
+    }
+}
